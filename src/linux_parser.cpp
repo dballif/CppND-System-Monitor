12a9280ac5b5dc1f9f032bc5f60e64ff44d2a8dd
Daniel Ballif <ballifdaniel@gmail.com>
2021-11-12 19:16:43 -0700

    Fixed a few of the LinuxParser class functions & call them from system.
    Fix LinuxParser::MemoryUtilization
    Fix LinuxParser::OperatingSystem
    Fix LinuxParser::RunningProcesses
    Fix LinuxParser::TotalProcesses
    Begin frix for LinuxParser::UpTime
    
    Call the above LinuxParser functions from system.cpp

--- src/linux_parser.cpp
+++ src/linux_parser.cpp
@@ -3,6 +3,7 @@
 #include <sstream>
 #include <string>
 #include <vector>
+#include <iostream>
 
 #include "linux_parser.h"
 
@@ -68,10 +69,48 @@ vector<int> LinuxParser::Pids() {
 }
 
 // TODO: Read and return the system memory utilization
-float LinuxParser::MemoryUtilization() { return 0.0; }
+float LinuxParser::MemoryUtilization() {
+  string line;
+  string key;
+  string value;
+  float MemTotal,MemFree,MemUsed;
+  std::ifstream filestream(kProcDirectory + kMeminfoFilename);
+  if (filestream.is_open()) {
+    while (std::getline(filestream, line)) {
+      std::replace(line.begin(), line.end(), ' ', '_');
+      std::replace(line.begin(), line.end(), '=', ' ');
+      std::replace(line.begin(), line.end(), '"', ' ');
+      std::replace(line.begin(), line.end(), ':', ' ');
+      std::istringstream linestream(line);
+      while (linestream >> key >> value) {
+        if (key == "MemTotal") {
+          std::replace(value.begin(), value.end(), '_', ' ');
+          MemTotal = std::stof(value);
+        }
+        if (key == "MemFree") {
+          std::replace(value.begin(), value.end(), '_', ' ');
+          MemFree = std::stof(value);
+        }
+      }
+      MemUsed = (MemTotal - MemFree)/MemTotal;
+    }
+  }
+  return MemUsed;
+}
 
 // TODO: Read and return the system uptime
-long LinuxParser::UpTime() { return 0; }
+long LinuxParser::UpTime() { 
+  string up_time, idle_time;
+  string line;
+  std::ifstream stream(kProcDirectory + kUptimeFilename);
+  if (stream.is_open()) {
+    std::getline(stream, line);
+    std::istringstream linestream(line);
+    linestream >> up_time >> idle_time;
+    return std::stol(up_time);
+  }
+  return std::stol(up_time);
+}
 
 // TODO: Read and return the number of jiffies for the system
 long LinuxParser::Jiffies() { return 0; }
@@ -90,10 +129,41 @@ long LinuxParser::IdleJiffies() { return 0; }
 vector<string> LinuxParser::CpuUtilization() { return {}; }
 
 // TODO: Read and return the total number of processes
-int LinuxParser::TotalProcesses() { return 0; }
+int LinuxParser::TotalProcesses() { 
+  string line;
+  string key;
+  string value;
+  std::ifstream filestream(kProcDirectory + kStatFilename);
+  if (filestream.is_open()) {
+    while (std::getline(filestream, line)) {
+      std::istringstream linestream(line);
+      while (linestream >> key >> value) {
+        if (key == "processes") {
+          return std::stoi(value);
+        }
+      }
+    }
+  }
+  return std::stoi(value);}
 
 // TODO: Read and return the number of running processes
-int LinuxParser::RunningProcesses() { return 0; }
+int LinuxParser::RunningProcesses() { 
+  string line;
+  string key;
+  string value;
+  std::ifstream filestream(kProcDirectory + kStatFilename);
+  if (filestream.is_open()) {
+    while (std::getline(filestream, line)) {
+      std::istringstream linestream(line);
+      while (linestream >> key >> value) {
+        if (key == "procs_running") {
+          std::replace(value.begin(), value.end(), '_', ' ');
+          return std::stoi(value);
+        }
+      }
+    }
+  }
+  return std::stoi(value);}
 
 // TODO: Read and return the command associated with a process
 // REMOVE: [[maybe_unused]] once you define the function
