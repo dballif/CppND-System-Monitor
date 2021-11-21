a3ad72bbf293e44350d382f5ed33de19f1e84ebe
Daniel Ballif <ballifdaniel@gmail.com>
2021-11-21 13:28:23 -0700

    Finish processor class
    Added calculation for overall cpu use

--- include/processor.h
+++ include/processor.h
@@ -1,12 +1,18 @@
 #ifndef PROCESSOR_H
+    #include <vector>
+    #include <string>
 #define PROCESSOR_H
 
 class Processor {
  public:
-  float Utilization();  // TODO: See src/processor.cpp
+  float Utilization();  // See src/processor.cpp
 
-  // TODO: Declare any necessary private members
+  // Declare any necessary private members
  private:
+    long Idle,PNIdle,NIdle,Total,totald,idled;
+    long PIdle;
+    long PTotal;
+    std::vector<std::string> cpuVals,cpuVals2;
 };
 
 #endif
--- src/processor.cpp
+++ src/processor.cpp
@@ -10,58 +10,37 @@ using std::vector;
 
 // Return the aggregate CPU utilization
 float Processor::Utilization() { 
-    vector<string> cpuVals,cpuVals2;
-    long Idle,PIdle,PNIdle,NIdle,PTotal,Total,totald,idled;
     float cpu;
     //Get all the values from the LinuxParser
     cpuVals = LinuxParser::CpuUtilization();
 
     //Read in the important values (convert them to a useful type)
-    long user = std::stol(cpuVals.at(0));
-    long nice = std::stol(cpuVals.at(1));
-    long system = std::stol(cpuVals.at(2));
-    long idle = std::stol(cpuVals.at(3));
-    long iowait = std::stol(cpuVals.at(4));
-    long irq = std::stol(cpuVals.at(5));
-    long softirq = std::stol(cpuVals.at(6));
-    long steal = std::stol(cpuVals.at(7));
+    long user = std::stol(cpuVals[0]);
+    long nice = std::stol(cpuVals[1]);
+    long system = std::stol(cpuVals[2]);
+    long idle = std::stol(cpuVals[3]);
+    long iowait = std::stol(cpuVals[4]);
+    long irq = std::stol(cpuVals[5]);
+    long softirq = std::stol(cpuVals[6]);
+    long steal = std::stol(cpuVals[7]);
     //Calculations don't use these
     //long guest = std::stol(cpuVals.at(8));
     //long guestnice = std::stol(cpuVals.at(9));
 
-    PIdle = idle +iowait;
-    PNIdle = user + nice + system + irq + softirq + steal;
-    PTotal = PIdle + PNIdle;
-
-    
-
-    //Grab New values
-    cpuVals2 = LinuxParser::CpuUtilization();
-
-    //Read in the important values (convert them to a useful type)
-    user = std::stol(cpuVals2.at(0));
-    nice = std::stol(cpuVals2.at(1));
-    system = std::stol(cpuVals2.at(2));
-    idle = std::stol(cpuVals2.at(3));
-    iowait = std::stol(cpuVals2.at(4));
-    irq = std::stol(cpuVals2.at(5));
-    softirq = std::stol(cpuVals2.at(6));
-    steal = std::stol(cpuVals2.at(7));
-    //Calculations don't use these
-    //guest = std::stol(cpuVals2.at(8));
-    //guestnice = std::stol(cpuVals2.at(9));
-
+    //Intermediate Calculations just make the final calculation cleaner
     Idle = idle +iowait;
     NIdle = user + nice + system + irq + softirq + steal;
     Total = Idle + NIdle;
 
     //Now do the calculations found on stack overflow (https://bit.ly/3xbfYMT)
-
     totald = Total - PTotal;
     idled = Idle - PIdle;
-    if (totald != 0){
-        cpu = (totald - idled)/totald;
-    }
-    else{cpu = 0.0;}
+
+    cpu = (totald - idled)/totald;
+
+    //Have to reset the values to the current values for the next calculation
+    PIdle = Idle;
+    PTotal = Total;
+
     return cpu;
  }
